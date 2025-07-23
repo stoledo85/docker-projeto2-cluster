3a740ed8389b0c1be748dad1edc63a19189551c1
Sander Toledo <sanderaugusto@gmail.com>
2025-07-23 19:08:45 -0300

    ajuste

--- master.sh
+++ master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker swarm init --advertise-addr=192.168.56.100
-sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+sudo docker swarm join-token worker -q | xargs -I{} echo docker swarm join --token {} 192.168.56.100:2377 > /vagrant/worker.sh
--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3hbx1ycs7wdjirvrvk7ua532pa2uq9xgtxgnpi7kk2eiv386d7-6fe4ftqwpe5dn79y1i4uihlz0 192.168.56.100:2377
+docker swarm join --token SWMTKN-1-47ae5h6raauteaygnxw8korqenj4aq7u06lu4307agqu61g0re-dpg3xfjwggq8111bn1nkor7cx 192.168.56.100:2377
